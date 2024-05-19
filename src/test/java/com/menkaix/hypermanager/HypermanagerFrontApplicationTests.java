e32d2bdcd6b449f313c953a36c7ad76ea0b13f09
menkaix <mendrika.irina@gmail.com>
2024-05-19 10:15:09 +0300

    init

---
+++ src/test/java/com/menkaix/hypermanager/HypermanagerFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menkaix.hypermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypermanagerFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
