95a6e97806be0c4972a91a94ea183c02e6b39937
menkaix <mendrika.irina@gmail.com>
2024-05-20 13:55:29 +0300

    ready

--- src/main/java/com/menkaix/hypermanager/controllers/StoryController.java
+++ src/main/java/com/menkaix/hypermanager/controllers/StoryController.java
@@ -1,5 +1,8 @@
 package com.menkaix.hypermanager.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.menkaix.hypermanager.models.ActorDTO;
 import com.menkaix.hypermanager.models.FullStoryDTO;
 import com.menkaix.hypermanager.models.Prompt;
 import com.menkaix.hypermanager.models.StoryDTO;
@@ -29,7 +33,22 @@ public class StoryController {
 		
 		FullStoryDTO storyDTO = storyService.getTree(storyID) ;
 		
+		List<ActorDTO> actors = new ArrayList<ActorDTO>() ;
+		
+		ActorDTO dto1 = new ActorDTO() ;
+		ActorDTO dto2 = new ActorDTO() ;
+		ActorDTO dto3 = new ActorDTO() ;
+		
+		dto1.id = "1" ; dto1.name = "alice" ;
+		dto2.id = "2" ; dto2.name = "bob" ;
+		dto3.id = "3" ; dto3.name = "dispatcher" ;
+		
+		actors.add(dto1);
+		actors.add(dto2);
+		actors.add(dto3);
+		
 		model.addAttribute("story", storyDTO);
+		model.addAttribute("actors", actors);
 		
 		return "story_details";
 	}
---
+++ src/main/java/com/menkaix/hypermanager/models/ActorDTO.java
@@ -0,0 +1,8 @@
+package com.menkaix.hypermanager.models;
+
+public class ActorDTO {
+	
+	public String id ;
+	public String name ;
+
+}
