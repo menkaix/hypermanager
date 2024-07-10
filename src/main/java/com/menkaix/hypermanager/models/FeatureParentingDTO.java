dcebccfcf35672cfd0717ae89649683297e12fce
menkaix <mendrika.irina@gmail.com>
2024-07-10 14:40:43 +0300

    setters and getters for parenting dto

--- src/main/java/com/menkaix/hypermanager/models/FeatureParentingDTO.java
+++ src/main/java/com/menkaix/hypermanager/models/FeatureParentingDTO.java
@@ -5,5 +5,25 @@ public class FeatureParentingDTO {
 	public String projectName ; 
 	public String parentID ;
 	public String childID ;
+	public String getProjectName() {
+		return projectName;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	public String getParentID() {
+		return parentID;
+	}
+	public void setParentID(String parentID) {
+		this.parentID = parentID;
+	}
+	public String getChildID() {
+		return childID;
+	}
+	public void setChildID(String childID) {
+		this.childID = childID;
+	}
+	
+	
 
 }
