9126a83a3ee86670a8071b58cd61a1ab3cb248c8
menkaix <mendrika.irina@gmail.com>
2024-07-11 13:07:20 +0300

    getters and setters again

--- src/main/java/com/menkaix/hypermanager/models/AbstractEntity.java
+++ src/main/java/com/menkaix/hypermanager/models/AbstractEntity.java
@@ -7,5 +7,26 @@ public abstract class AbstractEntity {
 	public String name;
 	public Date creationDate=new Date() ;
 	public Date lastUpdateDate ;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+	public Date getLastUpdateDate() {
+		return lastUpdateDate;
+	}
+	public void setLastUpdateDate(Date lastUpdateDate) {
+		this.lastUpdateDate = lastUpdateDate;
+	}
+	
+	
 
 }
