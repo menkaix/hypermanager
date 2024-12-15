bc27fd44e9c623b53a7d40da2e68afade8401f06
menkaix <mendrika.irina@gmail.com>
2024-12-15 19:21:52 +0300

    simplify

--- src/main/java/com/menkaix/hypermanager/HypermanagerApplication.java
+++ src/main/java/com/menkaix/hypermanager/HypermanagerApplication.java
@@ -2,8 +2,12 @@ package com.menkaix.hypermanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+		"com.menkaix.hypermanager.config",
+		"com.menkaix.hypermanager.controllers" })
 public class HypermanagerApplication {
 
 	public static void main(String[] args) {
