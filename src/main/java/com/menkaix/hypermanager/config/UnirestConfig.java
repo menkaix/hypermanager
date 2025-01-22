a11676e00778e38805829366cedfa61c89b272d7
menkaix <mendrika.irina@gmail.com>
2025-01-22 17:09:37 +0300

    configure Unirest

--- src/main/java/com/menkaix/hypermanager/config/UnirestConfig.java
+++ src/main/java/com/menkaix/hypermanager/config/UnirestConfig.java
@@ -1,8 +1,38 @@
 package com.menkaix.hypermanager.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import kong.unirest.core.Unirest;
+import kong.unirest.core.ObjectMapper;
 
 @Configuration
 public class UnirestConfig {
+
+    public UnirestConfig() {
+        configureUnirestObjectMapper();
+    }
+
+    private void configureUnirestObjectMapper() {
+        Gson gson = gson();
+        Unirest.config().setObjectMapper(new ObjectMapper() {
+            @Override
+            public <T> T readValue(String value, Class<T> valueType) {
+                return gson.fromJson(value, valueType);
+            }
+
+            @Override
+            public String writeValue(Object value) {
+                return gson.toJson(value);
+            }
+        });
+    }
+
+    @Bean
+    public Gson gson() {
+        return new GsonBuilder().setPrettyPrinting().create();
+    }
 
 }
